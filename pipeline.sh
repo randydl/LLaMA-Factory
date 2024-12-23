0059276361b2b394b0b7cacceb12b518aa7d0380
Hui Kang <hui.kang@cxmt.com>
2024-12-23 12:13:08 +0800

    Update config

---
+++ pipeline.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./run_ds.sh randy/llama3_pt.yaml
+
+sleep 10m
+
+./run_ds.sh randy/llama3_pt2.yaml
--- run_ds.sh
+++ run_ds.sh
@@ -2,7 +2,7 @@
 
 deepspeed \
     --num_gpus 8 \
-    --num_nodes 2 \
+    --num_nodes 4 \
     --hostfile hostfile \
     --master_addr 10.252.32.12 \
     randy/train.py "$@"
